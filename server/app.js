e18524b21c48c77d5b725bb9ee7fd8159fcab354
Sam Ewing <samuel.ewing@outlook.com>
2023-04-14 14:54:33 -0500

    add refreshToken controller

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
 const connectDB = require("./config/db");
@@ -16,6 +17,7 @@ connectDB();
 //middleware
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(cookieParser());
 app.use(cors());
 
 //routes
