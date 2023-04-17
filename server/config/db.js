ae2032e85b7bda29745642af12a97e71e57ae48c
Sam Ewing <samuel.ewing@outlook.com>
2023-04-17 16:32:15 -0500

    add test database connection

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,22 @@
 const mongoose = require("mongoose");
+const { MongoMemoryServer } = require("mongodb-memory-server");
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI);
-  } catch (err) {
-    console.log(err);
+  if (process.env.NODE_ENV !== "production") {
+    const mongodb = await MongoMemoryServer.create();
+    const uri = mongodb.getUri();
+    try {
+      await mongoose.connect(uri);
+      console.log("Connected to in-memory MongoDB");
+    } catch (err) {
+      console.error(err);
+    }
+  } else {
+    try {
+      await mongoose.connect(process.env.MONGODB_URI);
+    } catch (err) {
+      console.error(err);
+    }
   }
 };
 
