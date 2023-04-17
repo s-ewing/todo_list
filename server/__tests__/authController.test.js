1619f701336293030be62934151e8031a8910afd
Sam Ewing <samuel.ewing@outlook.com>
2023-04-17 18:44:31 -0500

    add handleLogin tests

--- server/__tests__/authController.test.js
+++ server/__tests__/authController.test.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 const { app } = require("../app");
 const User = require("../models/User");
 const { connectDB, disconnectDB } = require("../config/db");
-const { createNewUser } = require("../controllers/authController");
+const { createNewUser, handleLogin } = require("../controllers/authController");
 
 beforeAll(() => {
   connectDB();
@@ -15,10 +15,18 @@ afterAll(() => {
 afterAll(() => {});
 
 describe("createNewUser", () => {
-  it("should respond with status 400 if email or password is missing", async () => {
+  it("should respond with status 400 if email is missing", async () => {
     const res = await request(app)
       .post("/auth/register")
-      .send({ email: "test@test.com" });
+      .send({ email: "", password: "password" });
+    expect(res.status).toBe(400);
+    expect(res.body.message).toBe("Email and password are required");
+  });
+
+  it("should respond with status 400 if password is missing", async () => {
+    const res = await request(app)
+      .post("/auth/register")
+      .send({ email: "test@test.com", password: "" });
     expect(res.status).toBe(400);
     expect(res.body.message).toBe("Email and password are required");
   });
@@ -42,12 +50,14 @@ describe("createNewUser", () => {
   });
 
   it("should call next with an error if user creation fails", async () => {
-    const req = { body: {email: "new_email_2@test.com", password: "password" }};
+    const req = {
+      body: { email: "new_email_2@test.com", password: "password" },
+    };
 
     //stub res and next
     const res = {
       status: jest.fn(() => res),
-      json: jest.fn()
+      json: jest.fn(),
     };
     const next = jest.fn();
 
@@ -69,5 +79,84 @@ describe("createNewUser", () => {
 
     //restore User.create
     mockCreate.mockRestore();
+  });
+});
+
+describe("handleLogin", () => {
+  //user created in the createNewUser test with email: new_email@test.com and password: password
+
+  it("should respond with status 400 if email is missing", async () => {
+    const res = await request(app)
+      .post("/auth/login")
+      .send({ email: "", password: "password" });
+    expect(res.status).toBe(400);
+    expect(res.body.message).toBe("Email and password are required");
+  });
+
+  it("should respond with status 400 if password is missing", async () => {
+    const res = await request(app)
+      .post("/auth/login")
+      .send({ email: "test@test.com", password: "" });
+    expect(res.status).toBe(400);
+    expect(res.body.message).toBe("Email and password are required");
+  });
+
+  it("should respond with status 401 if email is invalid", async () => {
+    const res = await request(app)
+      .post("/auth/login")
+      .send({ email: "invalid@test.com", password: "password" });
+    expect(res.status).toBe(401);
+    expect(res.body.message).toBe("Invalid email or password");
+  });
+
+  it("should respond with status 401 if password is invalid", async () => {
+    const res = await request(app)
+      .post("/auth/login")
+      .send({ email: "test@test.com", password: "wrongpassword" });
+    expect(res.status).toBe(401);
+    expect(res.body.message).toBe("Invalid email or password");
+  });
+
+  it("should respond with status 200 and accessToken if credentials are valid", async () => {
+    const res = await request(app)
+      .post("/auth/login")
+      .send({ email: "new_email@test.com", password: "password" });
+    expect(res.status).toBe(200);
+    expect(res.body.accessToken).toBeDefined();
+  });
+
+  it('should store refreshToken in database and send it as a cookie if credentials are valid', async () => {
+    const res = await request(app)
+      .post('/auth/login')
+      .send({ email: 'new_email@test.com', password: 'password' });
+
+    expect(res.status).toBe(200);
+    const foundUser = await User.findOne({ email: "new_email@test.com" }).exec();
+    expect(foundUser.refreshToken).toBeDefined();
+    expect(res.headers['set-cookie']).toBeDefined();
+    expect(res.headers['set-cookie'][0]).toContain('refresh_token');
+  });
+
+  it('should call next with an error if an error occurs during database query', async () => {
+    const mockError = new Error('Database error occurred');
+    const mockFindOne = jest.spyOn(User, 'findOne');
+    mockFindOne.mockImplementationOnce(() => {
+      throw mockError;
+    });
+
+    const req = { body: { email: 'new_email@test.com', password: 'password' } };
+    const res = {
+      status: jest.fn(() => res),
+      json: jest.fn(),
+    };
+    const next = jest.fn();
+
+    await handleLogin(req, res, next);
+
+    expect(res.status).not.toHaveBeenCalled();
+    expect(res.json).not.toHaveBeenCalled();
+    expect(next).toHaveBeenCalledWith(mockError);
+
+    mockFindOne.mockRestore();
   });
 });
