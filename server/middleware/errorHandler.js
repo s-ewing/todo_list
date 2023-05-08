7efa5498ae0404a70fae47e7e18d490d4562fe6e
Sam Ewing <samuel.ewing@outlook.com>
2023-05-08 11:33:30 -0500

    change event/error log to not write file in production

--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -5,10 +5,12 @@ const errorHandler = (err, req, res) => {
   const origin = req.headers.origin ? req.headers.origin : "";
 
   //Log error
+  if (process.env.NODE_ENV !== "production") {
   logEvent(
     `${err.name}: ${err.message}\t${req.method}\t${req.url}\t${req.url}\t${origin}`,
     "errorLog.log"
   );
+  }
   console.log(err.stack);
 
   //Send response
--- server/middleware/logger.js
+++ server/middleware/logger.js
@@ -2,10 +2,12 @@ const { logEvent } = require("../utils/log");
 
 const logger = (req, res, next) => {
   //Check for origin
-  const origin = req.header.origin ? req.header.origin : '';
-  
+  const origin = req.header.origin ? req.header.origin : "";
+
   //Log event
-  logEvent(`${req.method}\t${req.url}\t${origin}`, "reqLog.log");
+  if (process.env.NODE_ENV !== "production") {
+    logEvent(`${req.method}\t${req.url}\t${origin}`, "reqLog.log");
+  }
   console.log(`${req.method} ${req.path}`);
 
   next();
